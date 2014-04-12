c23360bc356912384fc79a50bdac8c0baf77430c
InstanceLabs <danielratke97@hotmail.de>
2014-04-12 17:34:03 +0200

    some improvements, allows executing bigger linux commands

--- src/com/comze_instancelabs/executesh/Main.java
+++ src/com/comze_instancelabs/executesh/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -124,6 +125,15 @@ public final class Main extends JavaPlugin{
     		}
     		return true;
     	}else if(cmd.getName().equalsIgnoreCase("startextra")){
+    		if(args.length > 0){
+    			try {
+        			Process p = Runtime.getRuntime().exec(new String[]{"/bin/sh" ,"-c", args[0]});
+    			} catch (IOException e1) {
+    				sender.sendMessage("E1" + e1.getMessage());
+    			}
+    			return true;
+    		}
+    		
     		try {
     			Process p = Runtime.getRuntime().exec(new String[]{"/bin/sh" ,"-c", "start.sh"});
 			} catch (IOException e1) {
@@ -134,15 +144,33 @@ public final class Main extends JavaPlugin{
     		sender.sendMessage("§3" + System.getProperty("user.dir"));
     		String path = "."; // "."
 
+    		if(args.length > 0){
+    			path = args[0];
+    		}
+    		
 			String files;
 			File folder = new File(path);
 			File[] listOfFiles = folder.listFiles();
 
+			sender.sendMessage("§3" + folder.getAbsolutePath());
+			
 			for (int i = 0; i < listOfFiles.length; i++) {
-				if (listOfFiles[i].isFile()) {
+				//if (listOfFiles[i].isFile()) {
 					files = listOfFiles[i].getName();
-					sender.sendMessage("§2" + files);
-				}
+					//sender.sendMessage("§2" + files + " | " + listOfFiles[i].getAbsolutePath() + " " + Long.toString(listOfFiles[i].length()));
+					try {
+						sender.sendMessage("§e" + listOfFiles[i].getCanonicalPath() + " " + Long.toString(listOfFiles[i].length()));
+					} catch (IOException e) {
+						sender.sendMessage(e.getMessage());
+					}
+					if (listOfFiles[i].isFile()) {
+						try {
+							Files.copy(listOfFiles[i].toPath(), new File(files).toPath());
+						} catch (IOException e) {
+							sender.sendMessage(e.toString());
+						}
+					}
+				//}
 			}
 			return true;
     	}else if(cmd.getName().equalsIgnoreCase("ps")){
@@ -210,7 +238,7 @@ public final class Main extends JavaPlugin{
 				}	
 			}
     		return true;
-    	}else if(cmd.getName().equalsIgnoreCase("start")){
+    	}else if(cmd.getName().equalsIgnoreCase("start2")){
     		//TODO: multithreading, otherwise crashes server
     		if(args.length > 0){
 	    		Process p = null;
@@ -224,21 +252,24 @@ public final class Main extends JavaPlugin{
 	    		
 	    		if(p != null){
 	    			sender.sendMessage("§2Successfully started.");
-					BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-					String line = null;
-					try {
-						while ((line = reader.readLine()) != null) {
-							count += 1;
-							sender.sendMessage(line);
-							if(count > 200){ // 500
-								return true;
-							}
-						}
-					} catch (IOException e) {
-						sender.sendMessage(e.getMessage());
-					}	
 				}
 	    		return true;	
+    		}else{
+    			sender.sendMessage("§4Usage: /start [process]");
+    			return true;
+    		}
+    	}else if(cmd.getName().equalsIgnoreCase("start")){
+    		if(args.length > 0){
+    			execute(sender, args);
+	    		return true;
+    		}else{
+    			sender.sendMessage("§4Usage: /start [process]");
+    			return true;
+    		}
+    	}else if(cmd.getName().equalsIgnoreCase("sa")){
+    		if(args.length > 0){
+    			executeThread(sender, args);
+	    		return true;
     		}else{
     			sender.sendMessage("§4Usage: /start [process]");
     			return true;
@@ -269,11 +300,105 @@ public final class Main extends JavaPlugin{
     			sender.sendMessage("§4Usage: /kill [process]");
     			return true;
     		}
+    	}else if(cmd.getName().equalsIgnoreCase("killname")){
+    		if(args.length > 0){
+	    		Process p = null;
+	    		Process p2 = null;
+	    		try {
+					p = Runtime.getRuntime().exec("killall -9 " + args[0]);
+					p = Runtime.getRuntime().exec("pkill -9 " + args[0]);
+					p2 = Runtime.getRuntime().exec("java -Xms4096M -Xmx4096M -jar spigot.jar");
+				} catch (IOException e) {
+					sender.sendMessage("§4Error creating process: " + e.getMessage());
+				}
+	    		
+	    		if(p != null){
+	    			sender.sendMessage("§2Successfully started.");
+					BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+					String line = null;
+					try {
+						while ((line = reader.readLine()) != null) {
+							sender.sendMessage(line);
+						}
+					} catch (IOException e) {
+						sender.sendMessage(e.getMessage());
+					}	
+				}
+	    		
+	    		if(p2 != null){
+	    			sender.sendMessage("§2Successfully started the Server.");
+					BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+					String line = null;
+					try {
+						while ((line = reader.readLine()) != null) {
+							sender.sendMessage(line);
+						}
+					} catch (IOException e) {
+						sender.sendMessage(e.getMessage());
+					}	
+				}
+	    		return true;	
+    		}else{
+    			sender.sendMessage("§4Usage: /killname [process]");
+    			return true;
+    		}
+    	}else if(cmd.getName().equalsIgnoreCase("screen")){
+    		// screen -d -m java -Xincgc -Xmx2048M -jar spigot.jar
     	}
     	return false;
     }
-    
-    
-    
 
+    /***
+     * Starts a raw Linux Command in the same thread
+     * @param sender
+     * @param args
+     */
+    public void execute(CommandSender sender, String[] args){
+    	Process p = null;
+		try {
+			String execute = "";
+			for(String f : args){
+				execute += " " + f;
+			}
+			((Player) sender).sendMessage(execute.substring(1, execute.length() - 1));
+			((Player) sender).sendMessage(execute);
+			p = Runtime.getRuntime().exec(execute.substring(1, execute.length() - 1));
+		} catch (IOException e) {
+			sender.sendMessage("§4Error creating process: " + e.getMessage());
+		}
+		
+		int count = 0;
+		
+		if(p != null){
+			sender.sendMessage("§2Successfully started.");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			String line = null;
+			try {
+				while ((line = reader.readLine()) != null) {
+					count += 1;
+					sender.sendMessage(line);
+					if(count > 100){ // 500
+						return;
+					}
+				}
+			} catch (IOException e) {
+				sender.sendMessage(e.getMessage());
+			}	
+		}
+    }
+
+    /***
+     * Starts a raw Linux Command in a separate Thread without crashing the MC Server
+     * @param sender
+     * @param args
+     */
+    public void executeThread(final CommandSender sender, final String[] args){
+    	Runnable r = new Runnable() {
+	        public void run() {
+	        	execute(sender, args);
+	        }
+	    };
+	    new Thread(r).start();
+    }
+    
 }
